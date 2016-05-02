d39f3804d1de5a555d0234d1bd256c9d5b01953a
Chris Campbell <chris@change.org>
2016-05-02 09:26:55 -0700

    Beginnings of tests

---
+++ test/index.js
@@ -0,0 +1,34 @@
+var expect = require('chai').expect,
+  RedisKeyScanner = require('..');
+
+function noArgs() {
+  new RedisKeyScanner();
+}
+function noHost() {
+  new RedisKeyScanner({});
+}
+function noPort() {
+  new RedisKeyScanner({host: '127.0.0.1'});
+}
+
+describe('class RedisKeyScanner', function() {
+  describe('when instantiated with', function() {
+    describe('no arguments', function() {
+      it('throws a TypeError', function() {
+        expect(noArgs).to.throw(TypeError);
+      });
+    });
+
+    describe('no host', function() {
+      it('throws a TypeError', function() {
+        expect(noHost).to.throw(TypeError);
+      });
+    });
+
+    describe('no port', function() {
+      it('throws a TypeError', function() {
+        expect(noPort).to.throw(TypeError);
+      });
+    });
+  });
+});
