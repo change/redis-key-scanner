b5f47200b182b4560d15e54cc869e518cc92d6af
Chris Campbell <chris@change.org>
2016-05-11 17:06:45 -0700

    More tests

--- test.js
+++ test.js
@@ -1,33 +1,65 @@
-var expect = require('chai').expect,
+var _ = require('lodash'),
+  defaultArgs = {
+    host: 'localhost',
+    port: 6379
+  },
+  expect = require('chai').expect,
   RedisKeyScanner = require('.');
 
-function noArgs() {
-  new RedisKeyScanner();
+function extend(o) {
+  return function() {
+    new RedisKeyScanner(_.extend({}, defaultArgs, o));
+  };
 }
-function noHost() {
-  new RedisKeyScanner({});
-}
-function noPort() {
-  new RedisKeyScanner({host: '127.0.0.1'});
+function omit(keys) {
+  return function() {
+    new RedisKeyScanner(_.omit(defaultArgs, keys));
+  };
 }
 
 describe('class RedisKeyScanner', function() {
   describe('when instantiated with', function() {
     describe('no arguments', function() {
       it('throws a TypeError', function() {
-        expect(noArgs).to.throw(TypeError);
+        expect(function() { new RedisKeyScanner(); }).to.throw(TypeError);
       });
     });
 
     describe('no host', function() {
       it('throws a TypeError', function() {
-        expect(noHost).to.throw(TypeError);
+        expect(omit('host')).to.throw(TypeError);
       });
     });
 
     describe('no port', function() {
       it('throws a TypeError', function() {
-        expect(noPort).to.throw(TypeError);
+        expect(omit('port')).to.throw(TypeError);
+      });
+    });
+
+    describe('invalid timeframe', function() {
+      describe('`w` for max-idle', function() {
+        it('throws a TypeError', function() {
+          expect(extend({'max-idle': 'w'})).to.throw(TypeError);
+        });
+      });
+
+      describe('`foo` for max-ttl', function() {
+        it('throws a TypeError', function() {
+          expect(extend({'max-ttl': 'foo'})).to.throw(TypeError);
+        });
+      });
+
+      describe('`true` for min-idle', function() {
+        it('throws a TypeError', function() {
+          expect(extend({'min-idle': true})).to.throw(TypeError);
+        });
+      });
+
+      describe('`null` for min-ttl', function() {
+        it('throws a TypeError', function() {
+          expect(extend({'min-ttl': null})).to.throw(TypeError);
+        });
       });
     });
   });
